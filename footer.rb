ac07d9cf479b17cecd88c19c9c65cd164f7ad8bc
Amakuchisan <s18t318@stu.kagawa-u.ac.jp>
2018-08-22 14:55:13 +0900

    できてない

---
+++ footer.rb
@@ -0,0 +1,24 @@
+class Footer < TestObject
+  def initialize
+    # ダメージの蓄積量を表す変数
+    @damage_footer = 0
+    # floorの線
+    super(0, Window.height - 50, Image.new(800, 5, C_GREEN))
+  end
+
+  # enemyに攻撃されたときに増えるダメージ
+  def enemy_damage
+    if @enemy_bullets != nil
+      @enemy_bullets.each do |enemy_bullet|
+        # enemy_bullet.vanish if self===enemy_bullet
+        @damage_footer += 20 if @damage_footer <= 340
+      end
+    end
+  end
+
+  #ゲージを描画
+  def gage
+    Window.draw(8, 41, Image.new(14, 360, C_WHITE))
+    Window.draw(10, 400 - @damage_footer, Image.new(10, 1 + @damage_footer, C_RED))
+  end
+end
--- sample_main.rb
+++ sample_main.rb
@@ -3,12 +3,16 @@ require_relative 'testobject'
 require_relative 'player'
 require_relative 'enemy'
 require_relative 'bullet'
+require_relative 'footer'
 
 player = Player.new(400 - 32, 400 - 32, Image.new(64, 32, C_WHITE), nil)
 enemy = Enemy.new(400 - 64, 50, Image.new(128, 64, C_RED), nil)
+footer = Footer.new
 
 player.enemy_bullets = enemy.bullets
 enemy.enemy_bullets = player.bullets
+
+footer.enemy_bullets = enemy.bullets
 
 player_life = 3
 
@@ -16,6 +20,9 @@ Window.width = 800
 Window.height = 450
 
 Window.loop do
+  footer.draw
+  footer.gage
+  
   player.update
   player.draw
 
@@ -24,9 +31,13 @@ Window.loop do
 
   player.draw_bullets
   player.hit
-
+  
   enemy.draw_bullets
   enemy.hit
+  if enemy.shoot != nil
+    p enemy.shoot
+  end
+  footer.enemy_damage
 
   if player.vanished?
     Sprite.clean(player)
